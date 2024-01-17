6ed7493c28d04d9cd8deb0d4ee63623f13ce5960
Mihaly Adam <adammihaly05@gmail.com>
2024-01-17 18:47:40 +0100

    css valtoztatasok

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Nav from "./Nav";
 import './home.css';
 
 function Home()
@@ -8,7 +7,7 @@ function Home()
         <>
           <div className="home_conn">
             <h1>Admin Panel</h1> 
-            <p>Kedves csapat! </p>
+            <p>Kedves csapat! Meg szeretnénk kérni titeket, hogy az Adminpanelt annak rendeltetése szerint használjátok. Minden számotokra elérhető funkció logolva van, így mindent tudunk, hogy ki és milyen műveletett hajtott végre. A funkciók folyton bővülnek és egyes jogosultságokkal teljes kontrol is elérhető. </p>
            </div>  
         </>
     );
