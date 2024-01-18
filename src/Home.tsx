47856d95264ce44a374c09eceda26485780cb262
Mihaly Adam <adammihaly05@gmail.com>
2024-01-18 19:56:40 +0100

    Kesz kedolap
    Admin home kesz, plusz nav is telora optimalizalva

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,6 +8,20 @@ function Home()
           <div className="home_conn">
             <h1>Admin Panel</h1> 
             <p>Kedves csapat! Meg szeretnénk kérni titeket, hogy az Adminpanelt annak rendeltetése szerint használjátok. Minden számotokra elérhető funkció logolva van, így mindent tudunk, hogy ki és milyen műveletett hajtott végre. A funkciók folyton bővülnek és egyes jogosultságokkal teljes kontrol is elérhető. </p>
+              <div className="stats_grid">
+                <div className="stat_box">
+                  <h2>Regisztrációk</h2>
+                  <h3>2352</h3>
+                </div>
+                <div className="stat_box">
+                  <h2>Posztok</h2>
+                  <h3>1574</h3>
+                </div>
+                <div className="stat_box">
+                  <h2>Commentek</h2>
+                  <h3>958</h3>
+                </div>
+              </div>
            </div>  
         </>
     );
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -2,18 +2,41 @@ import './nav.css';
 import Home from './Home';
 import Posts from './Posts';
 import {Link, NavLink, Route, Routes} from 'react-router-dom';
+import React, { useState } from 'react';
 
 function Nav () 
 {
+
+  const [showDiv, setShowDiv] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowDiv(true);
+  };
+
+  const handleButtonClickClose = () => {
+    setShowDiv(false);
+  };
+
   return (
     <>
     <div className='grid'>
-      <div className='links'>
-        <NavLink to='/' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Home</NavLink>
-        <NavLink to='/posts' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Posts</NavLink>
+      <button onClick={handleButtonClick} className='mobile_menu'><i className="bi bi-list"></i></button>
+      <button onClick={handleButtonClickClose} className='mobile_menu_close' style={{ display: showDiv ? 'block' : 'none' }}><i className="bi bi-x-circle"></i></button>
+      
+      <div className='links_mobile' style={{  display: showDiv ? 'block' : 'none' } }>
+        <NavLink to='/' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Kezdőlap</NavLink>
+        <NavLink to='/posts' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Posztok</NavLink>
         <NavLink to='/admin' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Adminisztracio</NavLink>
         <NavLink to='/team' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Team</NavLink>
       </div>
+
+      <div className='links'>
+        <NavLink to='/' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Kezdőlap</NavLink>
+        <NavLink to='/posts' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Posztok</NavLink>
+        <NavLink to='/admin' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Adminisztracio</NavLink>
+        <NavLink to='/team' className={({ isActive, isPending }) => isPending ? "link" : isActive ? "link active" : "link"}>Team</NavLink>
+      </div>
+
       <div className='right'>
         <Routes>
           <Route path='/' element={<Home/>} />
