753d6e06eb9f34f8a9e09de654866cf29bdc5a5e
Mihaly Adam <adammihaly05@gmail.com>
2024-01-20 19:46:33 +0100

    poszt kezdes

--- src/Posts.tsx
+++ src/Posts.tsx
@@ -4,9 +4,24 @@ import './posts.css';
 function Post()
 {
     return(
-        <div>
-            sdsdsd
-        </div>
+        <>
+            <div className="posts_conn">
+                <h1>Posztok</h1>
+                <div className="posts_grid">
+                    <div className="post">
+                        <div className="post_data">
+                            <h2>58472</h2>
+                            <h3>Minecraft</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur odit error illo enim ducimus magnam alias cum fugit, iste repellat, dolore, culpa veritatis ipsum animi tempora repudiandae. Quidem, molestias quasi!</p>
+                            <h4>2024.01.22</h4>
+                        </div>
+                        <div className="post_act">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
