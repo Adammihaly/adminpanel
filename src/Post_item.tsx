2c607191ba2a39ed8eb8c1d3ca077dbbd72beece
Mihaly Adam <adammihaly05@gmail.com>
2024-02-06 19:30:10 +0100

    posz kezeles desin gepere kesz

--- src/Post_item.tsx
+++ src/Post_item.tsx
@@ -3,13 +3,19 @@ import { useState } from "react";
 interface PotsItemProps {
     showDiv: boolean;
     setShowDiv: React.Dispatch<React.SetStateAction<boolean>>;
+    showDiv2: boolean;
+    setShowDiv2: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function Pots_item({ showDiv, setShowDiv }: PotsItemProps)
+function Pots_item({ showDiv, setShowDiv, showDiv2, setShowDiv2 }: PotsItemProps)
 {
 
     const showButtonClick = () => {
         setShowDiv(true);
+    };
+
+    const showButtonClick2 = () => {
+        setShowDiv2(true);
     };
 
     return(
@@ -17,13 +23,15 @@ function Pots_item({ showDiv, setShowDiv }: PotsItemProps)
             <div className="post">
                 <div className="post_data">
                     <h2>58472</h2>
+                    <h5>A poszt törölve van</h5>
+                    <h6>A poszt fel van függesztve</h6>
                     <h3>Minecraft</h3>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur odit error illo enim ducimus magnam alias cum fugit, iste repellat, dolore, culpa veritatis ipsum animi tempora repudiandae. Quidem, molestias quasi!</p>
                     <h4>2024.01.22</h4>
                 </div>
                 <div className="post_act">
                     <button onClick={showButtonClick}>Törlés</button> <br />
-                    <button>Felfüggesztés</button> 
+                    <button onClick={showButtonClick2}>Felfüggesztés</button> 
                 </div>
             </div>
         </>
--- src/Posts.tsx
+++ src/Posts.tsx
@@ -11,13 +11,27 @@ function Post()
         setShowDiv(false);
     };
 
+    const [showDiv2, setShowDiv2] = useState<boolean>(false);
+
+    const megseButtonClick2 = () => {
+        setShowDiv2(false);
+    };
+
     return(
         <>
             <div className="posts_conn">
+
                 <h1>Posztok</h1>
+
                 <div className="posts_grid">
-                    <Post_item showDiv={showDiv} setShowDiv={setShowDiv} />
+                    <Post_item showDiv={showDiv} setShowDiv={setShowDiv} showDiv2={showDiv2} setShowDiv2={setShowDiv2} />
+                    <Post_item showDiv={showDiv} setShowDiv={setShowDiv} showDiv2={showDiv2} setShowDiv2={setShowDiv2} />
+                    <Post_item showDiv={showDiv} setShowDiv={setShowDiv} showDiv2={showDiv2} setShowDiv2={setShowDiv2} />
+                    <Post_item showDiv={showDiv} setShowDiv={setShowDiv} showDiv2={showDiv2} setShowDiv2={setShowDiv2} />
+                    <Post_item showDiv={showDiv} setShowDiv={setShowDiv} showDiv2={showDiv2} setShowDiv2={setShowDiv2} />
                 </div>
+
+
                 <div className="wrapper" style={{  display: showDiv ? 'flex' : 'none' } }>
                     <div className="popupdel">
                         <h2>Indokold meg a törlést</h2>
@@ -26,6 +40,16 @@ function Post()
                             <button>Végleges törlés</button><br />
                         </form> 
                         <button onClick={megseButtonClick}>Mégse</button>
+                    </div>
+                </div>
+                <div className="wrapper" style={{  display: showDiv2 ? 'flex' : 'none' } }>
+                    <div className="popupdel">
+                        <h2>Indokold meg a felfüggesztést</h2>
+                        <form>
+                            <input type="text" name="indok" placeholder="Indok" /><br />
+                            <button>Felfüggesztés</button><br />
+                        </form> 
+                        <button onClick={megseButtonClick2}>Mégse</button>
                     </div>
                 </div>
             </div>
